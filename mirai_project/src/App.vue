f66e616fb70123ad8dad89e93e2a96832c89a2f1
Nassim <latrach.nassim@gmail.com>
2026-06-30 15:22:53 +0200

    liste chapitre

--- mirai_project/src/App.vue
+++ mirai_project/src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+
+import Chapitre from './components/Chapitre.vue'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <HelloWorld />
+  <Chapitre />
 </template>
---
+++ mirai_project/src/components/Chapitre.vue
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<template>
+
+<h1 class=" title">Chapitre</h1>
+<div class="les chapitres">
+<a href>Chapitre 1</a>
+<p>Chapitre 2</p>
+<p>Chapitre 3</p>
+</div>
+
+</template>
+
+<style scope>
+.title {
+    display: flex;
+    justify-content: center;
+    text-transform: uppercase;
+
+}
+
+</style>
